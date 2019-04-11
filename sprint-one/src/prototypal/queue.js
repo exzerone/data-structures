81a5b91df9a1c862c6aac76c7944daac4cba6434
Chris Huang <chrishuang128@gmail.com>
2019-04-10 22:22:14 -0700

    Completed prototypal class for stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not not reference your old code in writing the new style.\
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  obj.head = 0;
+  obj.tail = 0;
+  obj.count = 0;
+  return obj;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value){
+  this.storage[this.tail] = value
+  this.count ++
+  this.tail ++
+}
 
+queueMethods.dequeue = function(){
+  var placeholder = this.storage[this.head];
+  if (this.head < this.tail){
+    delete this.storage[this.head]
+    this.head++
+  }
+  this.count -=1
+  return placeholder
+}
+
+queueMethods.size = function(){
+  if (this.count <= 0){
+    return 0
+  }
+  return this.count
+}
 
