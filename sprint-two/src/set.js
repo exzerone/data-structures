71008bb8bc992503e1064cbabff4c43d71ea9488
exzerone <chrishuang128@gmail.com>
2019-04-12 21:33:42 -0700

    Implemented and completed prototypal instantiation

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
+  //since object does not contain order, we use object
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //assign item to the object
+  this._storage[item] = item
 };
 
 setPrototype.contains = function(item) {
+  //look to see if our container has the item
+  //if we find the item in the container
+    //return true
+  //otherwise
+    //return false
+
+  if(this._storage[item]){
+    return true
+  }
+  return false
 };
 
 setPrototype.remove = function(item) {
+  //delete the item from the list
+  delete this._storage[item]
 };
 
 /*
